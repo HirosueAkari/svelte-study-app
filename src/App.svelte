9be427303f99bd67656f7644becf3ff84b36d0fb
HirosueAkari <hirosue_akari@retail-ai.jp>
2020-11-30 11:29:05 +0900

    画面遷移できるように

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
   import Router from 'svelte-spa-router'
   import Home from './components/pages/Home.svelte'
+  import Login from './components/pages/Login.svelte'
 
-  const routes = {
-    '/': Home
-  };
+  const routes: any = {
+    '/': Home,
+    '/login': Login
+  }
 </script>
 
 <body>
--- src/components/pages/Home.svelte
+++ src/components/pages/Home.svelte
@@ -1,3 +1,12 @@
 <main>
   <h1>Home</h1>
+  <button on:click={clickLogin}>Login</button>
 </main>
+
+<script lang="ts">
+import { push } from "svelte-spa-router"
+
+const clickLogin = () => {
+  push('/login')
+}
+</script>

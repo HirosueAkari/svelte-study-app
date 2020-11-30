32437777bf2ab32ca9c26966a8a0f15c1144f4f2
HirosueAkari <hirosue_akari@retail-ai.jp>
2020-11-30 16:49:44 +0900

    ボタンコンポーネントを利用する

--- src/components/atoms/Button.svelte
+++ src/components/atoms/Button.svelte
@@ -5,3 +5,16 @@ function clickBtn() {
   alert('ボタンクリック')
 }
 </script>
+<style lang="scss">
+button {
+  border-radius: 2px;
+  box-shadow: 0px 3px 1px -2px rgba(0,0,0,0.2),
+    0px 2px 2px 0px rgba(0,0,0,0.14),
+    0px 1px 5px 0px rgba(0,0,0,0.12);
+  color: white;
+  background-color: #1867c0;
+  border-color: #1867c0;
+  padding: 0 16px;
+  height: 36px;
+}
+</style>
--- src/components/pages/Login.svelte
+++ src/components/pages/Login.svelte
@@ -4,16 +4,12 @@
     <div>
       <input type="text" class="input" placeholder="E-mail">
       <input type="password" class="input" placeholder="Password">
-      <button on:click={clickLogin} class="login-btn">LOGIN</button>
+      <Button />
     </div>
   </div>
 </main>
 <script lang="ts">
-import { push } from "svelte-spa-router"
-
-const clickLogin = () => {
-  push('/home')
-}
+import Button from '../atoms/Button.svelte'
 </script>
 <style lang="scss">
 .input {
@@ -31,16 +27,5 @@ const clickLogin = () => {
 }
 h1 {
   padding: 16px;
-}
-.login-btn {
-  border-radius: 2px;
-  box-shadow: 0px 3px 1px -2px rgba(0,0,0,0.2),
-    0px 2px 2px 0px rgba(0,0,0,0.14),
-    0px 1px 5px 0px rgba(0,0,0,0.12);
-  color: white;
-  background-color: #1867c0;
-  border-color: #1867c0;
-  padding: 0 16px;
-  height: 36px;
 }
 </style>
